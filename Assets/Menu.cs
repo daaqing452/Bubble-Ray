59d6c6050ff0ae4957a4a938063d344cdaa885fe
Yiqin Lu <daaqing452@gmail.com>
2017-01-17 11:12:38 +0800

    init

---
+++ Assets/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    GameObject button_BubbleRay_BubbleVisible;
+    GameObject button_BubbleRay_Center;
+
+    // Use this for initialization
+    void Start() {
+        button_BubbleRay_BubbleVisible = GameObject.Find("Menu/Bubble Ray/Button Bubble Visible");
+        button_BubbleRay_Center = GameObject.Find("Menu/Bubble Ray/Button Center");
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void ClearButtonVisibility() {
+        button_BubbleRay_BubbleVisible.SetActive(false);
+        button_BubbleRay_Center.SetActive(false);
+    }
+
+    public void OnClick_BubbleRay() {
+        ClearButtonVisibility();
+        button_BubbleRay_BubbleVisible.SetActive(true);
+        button_BubbleRay_Center.SetActive(true);
+    }
+
+    public void OnClick_BubbleRay_BubbleVisible() {
+        bool visible = Play.bubbleRayBubbleVisible;
+        Text text = button_BubbleRay_BubbleVisible.transform.GetChild(0).GetComponent<Text>();
+        if (visible == true) {
+            Play.bubbleRayBubbleVisible = false;
+            text.text = "Bubble Visible";
+        } else {
+            Play.bubbleRayBubbleVisible = true;
+            text.text = "Bubble Invisible";
+        }
+    }
+
+    public void OnClick_BubbleRay_Center() {
+
+    }
+}
---
+++ Assets/Play.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+    public static bool bubbleRayBubbleVisible = false;
+
+    GameObject controllerLeft;
+    GameObject controllerRight;
+    GameObject controller;
+    GameObject[] playProps;
+    GameObject bubble;
+    GameObject ray;
+
+    // Use this for initialization
+    void Start () {
+        controllerLeft = GameObject.Find("VROrigin/[CameraRig]/Controller (left)");
+        controllerRight = GameObject.Find("VROrigin/[CameraRig]/Controller (right)");
+        controller = controllerRight;
+        playProps = GameObject.FindGameObjectsWithTag("PlayProp");
+        bubble = GameObject.Find("Bubble Ray/Bubble");
+        ray = GameObject.Find("Bubble Ray/Ray");
+        ray.transform.SetParent(controller.transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 p = controller.transform.position;
+        Vector3 v = QuaternionToVector(controller.transform.rotation);
+        // indicator.transform.position = p + v * 5;
+        GameObject selectObject = null;
+        float minDist = 1e20f;
+        Vector3 intersectionPoint = new Vector3(0, 0, -5);
+        foreach (GameObject g in playProps) {
+            g.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+            Vector3 q = g.transform.position;
+            float t = -(p.x + p.y + p.z - q.x - q.y - q.z) / (v.x + v.y + v.z);
+            Vector3 i = p + v * t;
+            float d = (q - i).magnitude - g.transform.localScale.x / 2;
+            if (d < minDist) {
+                minDist = d;
+                selectObject = g;
+                intersectionPoint = i;
+            }
+        }
+        if (bubbleRayBubbleVisible) {
+            bubble.transform.position = intersectionPoint;
+            bubble.transform.localScale = new Vector3(1, 1, 1) * minDist * 2;
+        } else {
+            bubble.transform.position = new Vector3(0, 0, -5);
+            bubble.transform.localScale = new Vector3(1, 1, 1);
+        }
+        selectObject.GetComponent<Renderer>().material.color = new Color(0, 1, 1);
+    }
+
+    Vector3 QuaternionToVector(Quaternion q) {
+        Vector3 r = q.eulerAngles / 180.0f * Mathf.Acos(-1);
+        float dx = Mathf.Cos(r.x) * Mathf.Sin(r.y);
+        float dy = -Mathf.Sin(r.x);
+        float dz = Mathf.Cos(r.x) * Mathf.Cos(r.y);
+        return new Vector3(dx, dy, dz);
+    }
+}
