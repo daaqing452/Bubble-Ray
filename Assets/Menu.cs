6c07d6eb09c834b4529b4877f72d6283ec0a0210
Yiqin Lu <daaqing452@gmail.com>
2017-01-17 15:42:58 +0800

    add plane view bubble ray

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     GameObject button_BubbleRay_BubbleVisible;
-    GameObject button_BubbleRay_Center;
+    GameObject button_BubbleRay_HandCentered;
+    GameObject button_BubbleRay_PlaneView;
 
     // Use this for initialization
     void Start() {
-        button_BubbleRay_BubbleVisible = GameObject.Find("Menu/Bubble Ray/Button Bubble Visible");
-        button_BubbleRay_Center = GameObject.Find("Menu/Bubble Ray/Button Center");
+        button_BubbleRay_BubbleVisible = GameObject.Find("Menu/Button Bubble Visible");
+        button_BubbleRay_HandCentered = GameObject.Find("Menu/Bubble Ray/Button Hand Centered");
+        button_BubbleRay_PlaneView = GameObject.Find("Menu/Bubble Ray/Button Plane View");
     }
 
     // Update is called once per frame
@@ -19,29 +21,33 @@ public class Menu : MonoBehaviour
     }
 
     void ClearButtonVisibility() {
-        button_BubbleRay_BubbleVisible.SetActive(false);
-        button_BubbleRay_Center.SetActive(false);
+        button_BubbleRay_HandCentered.SetActive(false);
+        button_BubbleRay_PlaneView.SetActive(false);
     }
 
     public void OnClick_BubbleRay() {
         ClearButtonVisibility();
-        button_BubbleRay_BubbleVisible.SetActive(true);
-        button_BubbleRay_Center.SetActive(true);
+        button_BubbleRay_HandCentered.SetActive(true);
+        button_BubbleRay_PlaneView.SetActive(true);
     }
 
     public void OnClick_BubbleRay_BubbleVisible() {
-        bool visible = Play.bubbleRayBubbleVisible;
+        bool visible = Play.bubbleVisible;
         Text text = button_BubbleRay_BubbleVisible.transform.GetChild(0).GetComponent<Text>();
         if (visible == true) {
-            Play.bubbleRayBubbleVisible = false;
+            Play.bubbleVisible = false;
             text.text = "Bubble Visible";
         } else {
-            Play.bubbleRayBubbleVisible = true;
+            Play.bubbleVisible = true;
             text.text = "Bubble Invisible";
         }
     }
 
-    public void OnClick_BubbleRay_Center() {
+    public void OnClick_BubbleRay_HandCentered() {
+        Play.bubbleRay_Method = "hand centered";
+    }
 
+    public void OnClick_BubbleRay_PlaneView() {
+        Play.bubbleRay_Method = "plane view";
     }
 }
--- Assets/Play.cs
+++ Assets/Play.cs
@@ -4,54 +4,91 @@ using UnityEngine;
 
 public class Play : MonoBehaviour {
 
-    public static bool bubbleRayBubbleVisible = false;
+    public static bool bubbleVisible = false;
+    public static string bubbleRay_Method = "hand centered";
 
     GameObject controllerLeft;
     GameObject controllerRight;
     GameObject controller;
-    GameObject[] playProps;
-    GameObject bubble;
     GameObject ray;
+    GameObject[] playProps;
+    GameObject bubbleRay_BubbleBall;
+    GameObject bubbleRay_BubbleCircle;
+
+    GameObject selectedObject;
 
     // Use this for initialization
     void Start () {
         controllerLeft = GameObject.Find("VROrigin/[CameraRig]/Controller (left)");
         controllerRight = GameObject.Find("VROrigin/[CameraRig]/Controller (right)");
         controller = controllerRight;
-        playProps = GameObject.FindGameObjectsWithTag("PlayProp");
-        bubble = GameObject.Find("Bubble Ray/Bubble");
-        ray = GameObject.Find("Bubble Ray/Ray");
+        ray = GameObject.Find("Ray");
+        playProps = GameObject.FindGameObjectsWithTag("play prop");
+        bubbleRay_BubbleBall = GameObject.Find("Bubble Ray/Bubble Ball");
+        bubbleRay_BubbleCircle = GameObject.Find("Bubble Ray/Bubble Circle");
         ray.transform.SetParent(controller.transform);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        BubbleRay();
+    }
+
+    void BubbleRay()
+    {
+        SelectObject(selectedObject, 0);
         Vector3 p = controller.transform.position;
         Vector3 v = QuaternionToVector(controller.transform.rotation);
-        // indicator.transform.position = p + v * 5;
-        GameObject selectObject = null;
         float minDist = 1e20f;
-        Vector3 intersectionPoint = new Vector3(0, 0, -5);
-        foreach (GameObject g in playProps) {
+        switch (bubbleRay_Method) {
+            case "hand centered":
+                Vector3 intersectPoint = new Vector3(0, 0, -5);
+                foreach (GameObject g in playProps) {
+                    Vector3 q = g.transform.position;
+                    float t = -(p.x + p.y + p.z - q.x - q.y - q.z) / (v.x + v.y + v.z);
+                    Vector3 i = p + v * t;
+                    float d = (q - i).magnitude - g.transform.localScale.x / 2;
+                    if (d < minDist) {
+                        minDist = d;
+                        selectedObject = g;
+                        intersectPoint = i;
+                    }
+                }
+                Vector3 renderPoint = (bubbleVisible) ? intersectPoint : new Vector3(0, 0, -5);
+                float renderScale = (bubbleVisible) ? minDist * 2 : 1;
+                bubbleRay_BubbleBall.transform.position = renderPoint;
+                bubbleRay_BubbleBall.transform.localScale = new Vector3(1, 1, 1) * renderScale;
+                break;
+                
+            case "plane view":
+                Vector3 pointInDepth = new Vector3(0, 0, -5);
+                foreach (GameObject g in playProps) {
+                    Vector3 q = g.transform.position;
+                    float t = (q.z - p.z) / v.z;
+                    Vector3 i = p + v * t;
+                    float d = (q - i).magnitude - g.transform.localScale.x / 2;
+                    if (d < minDist) {
+                        minDist = d;
+                        selectedObject = g;
+                        pointInDepth = i;
+                    }
+                }
+                renderPoint = (bubbleVisible) ? pointInDepth : new Vector3(0, 0, -5);
+                renderScale = (bubbleVisible) ? minDist * 2 : 1;
+                bubbleRay_BubbleBall.transform.position = renderPoint;
+                bubbleRay_BubbleBall.transform.localScale = new Vector3(1, 1, 0.01f) * renderScale;
+                break;
+        }
+        SelectObject(selectedObject, 1);
+    }
+
+    void SelectObject(GameObject g, int status) {
+        if (g == null) return;
+        if (status == 0) {
             g.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
-            Vector3 q = g.transform.position;
-            float t = -(p.x + p.y + p.z - q.x - q.y - q.z) / (v.x + v.y + v.z);
-            Vector3 i = p + v * t;
-            float d = (q - i).magnitude - g.transform.localScale.x / 2;
-            if (d < minDist) {
-                minDist = d;
-                selectObject = g;
-                intersectionPoint = i;
-            }
-        }
-        if (bubbleRayBubbleVisible) {
-            bubble.transform.position = intersectionPoint;
-            bubble.transform.localScale = new Vector3(1, 1, 1) * minDist * 2;
         } else {
-            bubble.transform.position = new Vector3(0, 0, -5);
-            bubble.transform.localScale = new Vector3(1, 1, 1);
+            g.GetComponent<Renderer>().material.color = new Color(0, 1, 1);
         }
-        selectObject.GetComponent<Renderer>().material.color = new Color(0, 1, 1);
     }
 
     Vector3 QuaternionToVector(Quaternion q) {
